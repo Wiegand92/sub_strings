c18a096d42780f73f1af75e2e0637ea109cf9fe0
wiegand92 <wiegand.alex@gmail.com>
2021-05-02 18:27:45 -0400

    pass different word test

--- lib/sub_strings.rb
+++ lib/sub_strings.rb
@@ -1,3 +1,12 @@
 def sub_strings(string, dictionary)
-  return { "or" => 1, "for" => 1, "get" => 1}
+  words = string.split(/\W?\s/)
+  found_hash = Hash.new
+  words.each { |word| 
+    dictionary.each { |dictionary_word| 
+      if word.include?(dictionary_word)
+        found_hash[dictionary_word] != nil ? found_hash[dictionary_word] += 1 : found_hash[dictionary_word] = 1 
+      end
+    }
+  }
+  return found_hash
 end
--- spec/sub_strings_spec.rb
+++ spec/sub_strings_spec.rb
@@ -1,11 +1,20 @@
 require './lib/sub_strings.rb'
 
 describe "#sub_strings" do
-  dictionary = ["or", "for", "get"]
-  string = "forget"
   it "returns a hash listing each substring (case insensitive), and how many times it was found" do
+    dictionary = ["or", "for", "get"]
+    string = "forget"
     result = sub_strings(string, dictionary)
     expected = {"or" => 1, "for" => 1, "get" => 1}
+
+    expect(result).to eql(expected)
+  end
+
+  it "only returns the proper hash" do
+    dictionary = ["do", "on"]
+    string = "dont"
+    expected = { "do" => 1, "on" => 1}
+    result = sub_strings(string, dictionary)
 
     expect(result).to eql(expected)
   end

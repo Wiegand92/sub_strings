08b4236d53b5a61fc3345e393a624380c033183b
wiegand92 <wiegand.alex@gmail.com>
2021-05-02 18:47:15 -0400

    handle edge cases

--- lib/sub_strings.rb
+++ lib/sub_strings.rb
@@ -3,8 +3,8 @@ def sub_strings(string, dictionary)
   found_hash = Hash.new
   words.each { |word| 
     dictionary.each { |dictionary_word| 
-      if word.include?(dictionary_word)
-        found_hash[dictionary_word] != nil ? found_hash[dictionary_word] += 1 : found_hash[dictionary_word] = 1 
+      if word.downcase.include?(dictionary_word.downcase)
+        found_hash[dictionary_word] != nil ? found_hash[dictionary_word] += 1 : found_hash[dictionary_word] = 1
       end
     }
   }
--- spec/sub_strings_spec.rb
+++ spec/sub_strings_spec.rb
@@ -1,20 +1,33 @@
 require './lib/sub_strings.rb'
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 describe "#sub_strings" do
   it "returns a hash listing each substring (case insensitive), and how many times it was found" do
-    dictionary = ["or", "for", "get"]
-    string = "forget"
-    result = sub_strings(string, dictionary)
-    expected = {"or" => 1, "for" => 1, "get" => 1}
+    result = sub_strings("below", dictionary)
+    expected = { "below" => 1, "low" => 1 }
+
+    expect(result).to eql(expected)
+  end
+
+  it "is case insensitve" do
+    result = sub_strings("Below", dictionary)
+    expected = { "below" => 1, "low" => 1 }
 
     expect(result).to eql(expected)
   end
 
   it "only returns the proper hash" do
-    dictionary = ["do", "on"]
-    string = "dont"
+    other_dictionary = ["do", "on"]
     expected = { "do" => 1, "on" => 1}
-    result = sub_strings(string, dictionary)
+    result = sub_strings("dont", other_dictionary)
+
+    expect(result).to eql(expected)
+  end
+
+  it "handles multiple words" do
+    result = sub_strings("Howdy partner, sit down! How's it going?", dictionary)
+    expected = { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
 
     expect(result).to eql(expected)
   end
